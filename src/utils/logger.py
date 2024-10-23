0237fbf14735e6eddba9952bb8be42e1f00156de
IMISSHER99 <145341240+IMISSHER99@users.noreply.github.com>
2024-10-23 16:43:34 +0530

    Changed Logger to not send logs to cloud logging when running tests

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,27 @@
 import logging
+import os
 from google.cloud.logging_v2 import Client
 from google.cloud.logging_v2.handlers import CloudLoggingHandler
 
-client = Client()
-cloud_handler = CloudLoggingHandler(client)
+from src.config.constants import ENVIRONMENT, TEST
 
-logging.getLogger().setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
-logger.addHandler(cloud_handler)
+
+def setup_logging():
+    # Only setup cloud logging if NOT in a test environment
+    if os.getenv(ENVIRONMENT) != TEST:
+        client = Client()
+        cloud_handler = CloudLoggingHandler(client)
+
+        log = logging.getLogger(__name__)
+        log.setLevel(logging.INFO)
+        log.addHandler(cloud_handler)
+    else:
+        log = logging.getLogger(__name__)
+        log.setLevel(logging.DEBUG)  # Set to DEBUG or INFO for test logs
+        log.addHandler(logging.StreamHandler())  # Output logs to console during tests
+
+    return log
+
+
+# Initialize logger when needed
+logger = setup_logging()
